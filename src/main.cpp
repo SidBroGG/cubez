2230bd873016c8d5cb05d5eb9758bd8230f26e15
sidbro_archpc <sasha.sidenko09@gmail.com>
2025-12-15 16:20:18 +0300

    some code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main() {
-    std::cout << "Very cool app";
+    if (!glfwInit()) {
+        throw std::runtime_error("Failed to init glfw lib");
+    }
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "New window", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
 }
