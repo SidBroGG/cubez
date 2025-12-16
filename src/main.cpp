368522a484779b46577a7b866def6b8c72b8d767
sidbro_archpc <sasha.sidenko09@gmail.com>
2025-12-16 16:43:27 +0300

    resrtructured code

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,60 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_vulkan.h>
+#include <vulkan/vulkan.h>
+#include <vector>
 #include <iostream>
-#include <GLFW/glfw3.h>
+
+class App {
+public:
+    App() {
+        if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+            throw std::runtime_error(SDL_GetError());
+        }
+    }
+
+    ~App() {
+
+    }
+
+    void Init() {
+
+    }
+
+    void Run() {
+
+    }
+
+private:
+    SDL_Window* window = nullptr;
+    VkInstance instance = VK_NULL_HANDLE;
+    VkSurfaceKHR surface = VK_NULL_HANDLE;
+
+    void CreateWindow() {
+        window = SDL_CreateWindow(
+            "Cubez",
+            800,
+            600,
+            SDL_WINDOW_VULKAN
+        );
+
+        if (!window) {
+            throw std::runtime_error(SDL_GetError());
+        }
+    }
+
+    void CreateVulkanInstance() {
+        uint32_t extCount = 0;
+        if (!SDL_Vulkan_GetInstanceExtensions(&extCount)) {
+            throw std::runtime_error("Failed to get vulkan extensions");
+        }
+        
+    }
+};
 
 int main() {
-    if (!glfwInit()) {
-        throw std::runtime_error("Failed to init glfw lib");
-    }
+    App app{};
+    app.Init();
+    app.Run();
 
-    GLFWwindow* window = glfwCreateWindow(640, 480, "New window", NULL, NULL);
-    if (!window) {
-        glfwTerminate();
-        exit(EXIT_FAILURE);
-    }
+    return 0;
 }
